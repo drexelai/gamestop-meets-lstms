09524df3af6d556eb95e36cf06ea83bfac459fac
ya332 <alparslanyigitcan@gmail.com>
2021-02-03 10:28:46 -0500

    Enhance requirements.txt, add model load and save utility functions

--- main.py
+++ main.py
@@ -9,24 +9,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import os
-
-
-
-df = pd.read_csv(os.path.join('data', 'S&P_500_Index_Data.csv'), parse_dates=['date'])
-df.head()
-# adapted from https://github.com/Tekraj15/AnomalyDetectionTimeSeriesData/blob/master/Anomaly_Detection_Time_Series_Keras.ipynb
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=df.date, y=df.close,
-                    mode='lines',
-                    name='close'))
-fig.update_layout(showlegend=True)
-fig.show()
-
-# preprocess
-train_size = int(len(df) * 0.8)
-test_size = len(df) - train_size
-train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
-print(train.shape, test.shape)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
+from sklearn.preprocessing import StandardScaler
 
 def create_dataset(X, y, time_steps=1):
     Xs, ys = [], []
@@ -36,47 +21,6 @@ def create_dataset(X, y, time_steps=1):
         ys.append(y.iloc[i + time_steps])
     return np.array(Xs), np.array(ys)
 
-
-# Go back 30 days for now
-time_steps = 30
-
-X_train, y_train = create_dataset(train[['close']], train.close, time_steps)
-X_test, y_test = create_dataset(test[['close']], test.close, time_steps)
-
-print(X_train.shape)
-
-timesteps = X_train.shape[1]
-num_features = X_train.shape[2]
-
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
-
-model = Sequential([
-    LSTM(128, activation='relu', input_shape=(timesteps, num_features)),
-    RepeatVector(timesteps),
-    LSTM(128, activation='relu', return_sequences=True),
-    Dropout(0.2),
-    TimeDistributed(Dense(num_features))                 
-])
-
-# This is not a classification problem, but a regression problem. We can't use accuracy. 
-# We have to use mean squared error on each prediction.
-model.compile(loss='mae', optimizer='adam')
-print(model.summary())
-
-
-es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min')
-history = model.fit(
-    X_train, y_train,
-    epochs=100,
-    batch_size=32,
-    validation_split=0.1,
-    callbacks = [es],
-    shuffle=False
-)
-
-print(history.history)
 def plot_loss(history):
 	# Plot training & validation loss values
 	plt.plot(history.history['loss'])
@@ -87,24 +31,40 @@ def plot_loss(history):
 	plt.legend(['Train', 'Test'], loc='upper left')
 	plt.show()
 
-plot_loss_and_accuracy(history)
+def build_model(timesteps, num_features):
+    model = Sequential([
+        LSTM(128, activation='relu', input_shape=(timesteps, num_features)),
+        RepeatVector(timesteps),
+        LSTM(128, activation='relu', return_sequences=True),
+        Dropout(0.2),
+        TimeDistributed(Dense(num_features))                 
+    ])
 
-X_train_pred = model.predict(X_train)
-
-train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
-print("train_mae_loss", train_mae_loss)
-print(model.evaluate(X_test, y_test))
-
-sns.distplot(train_mae_loss, bins=50, kde=True)
-
-X_test_pred = model.predict(X_test)
-
-test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
-print("test_mae_loss", test_mae_loss)
-sns.distplot(test_mae_loss, bins=50, kde=True)
-plt.show()
+    # This is not a classification problem, but a regression problem. We can't use accuracy. 
+    # We have to use mean squared error on each prediction.
+    model.compile(loss='mae', optimizer='adam')
+    print(model.summary())
+    return model
 
 
+def load_model(model_file_name):
+    print("[INFO] loading {} model".format(model_file_name))
+    json_file = open(model_file_name + ".json", 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(model_file_name + ".h5")
+    loaded_model.compile(loss='mae', optimizer='adam')
+    return loaded_model
+
+def save_model(model, model_file_name):
+    print("[INFO] Saving {} model".format(model_file_name))
+    model_json = model.to_json()
+    with open(model_file_name +".json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(model_file_name + ".h5")
 
 def detect_anomaly(test, time_steps):
     THRESHOLD = 0.55
@@ -130,6 +90,11 @@ def detect_anomaly(test, time_steps):
     anomalies = test_score_df[test_score_df.anomaly == True]
     anomalies.head()
 
+    scaler = StandardScaler()
+    scaler = scaler.fit(train[['close']])
+
+    train['close'] = scaler.transform(train[['close']])
+    test['close'] = scaler.transform(test[['close']])
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=test[time_steps:].date, y=scaler.inverse_transform(test[time_steps:].close),
@@ -141,5 +106,65 @@ def detect_anomaly(test, time_steps):
     fig.update_layout(showlegend=True)
     fig.show()
 
+if __name__ == "__main__":
 
-detect_anomaly(test, time_steps)
+    # read data
+    df = pd.read_csv(os.path.join('data', 'S&P_500_Index_Data.csv'), parse_dates=['date'])
+
+    # adapted from https://github.com/Tekraj15/AnomalyDetectionTimeSeriesData/blob/master/Anomaly_Detection_Time_Series_Keras.ipynb
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.date, y=df.close,
+                        mode='lines',
+                        name='close'))
+    fig.update_layout(showlegend=True)
+    fig.show()
+
+    # preprocess
+    train_size = int(len(df) * 0.8)
+    test_size = len(df) - train_size
+    train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
+    print(train.shape, test.shape)
+
+    # Go back 30 days for now
+    time_steps = 30
+
+    X_train, y_train = create_dataset(train[['close']], train.close, time_steps)
+    X_test, y_test = create_dataset(test[['close']], test.close, time_steps)
+
+    print(X_train.shape)
+
+    timesteps = X_train.shape[1]
+    num_features = X_train.shape[2]
+
+    # Fit model with early stopping
+    model = build_model(time_steps, num_features)
+
+    es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min')
+
+    history = model.fit(
+        X_train, y_train,
+        epochs=100,
+        batch_size=32,
+        validation_split=0.1,
+        callbacks = [es],
+        shuffle=False
+    )
+
+    plot_loss(history)
+
+    X_train_pred = model.predict(X_train)
+
+    train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
+    print("train_mae_loss", train_mae_loss)
+    print(model.evaluate(X_test, y_test))
+
+    sns.distplot(train_mae_loss, bins=50, kde=True)
+
+    X_test_pred = model.predict(X_test)
+
+    test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
+    print("test_mae_loss", test_mae_loss)
+    sns.distplot(test_mae_loss, bins=50, kde=True)
+    plt.show()
+
+    detect_anomaly(test, time_steps)
