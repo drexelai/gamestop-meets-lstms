d182142e6bbe1b500ecdd8b8c89350cb306e08ad
ya332 <alparslanyigitcan@gmail.com>
2021-02-14 17:16:58 -0500

    Add fetch data functionality, add lstm predictions and detect anomaly utility functions

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
 from sklearn.preprocessing import StandardScaler
-import datetime
-
+from datetime import date, datetime
+import yfinance as yf
 import streamlit as st
 
 def create_dataset(X, y, time_steps=1):
@@ -73,72 +73,76 @@ def save_model(model, model_file_name):
     # serialize weights to HDF5
     model.save_weights(model_file_name + ".h5")
 
-def detect_anomaly(test, time_steps=1):
+def detect_anomaly(model, train, test, X_train, X_test, time_steps=1, close_column_name="Adj Close"):
     THRESHOLD = 0.55
+
+    test_mae_loss = calculate_mae_loss(model, X_test)
+    print("[INFO] mae loss:", test_mae_loss)
 
     test_score_df = pd.DataFrame(test[time_steps:])
     test_score_df['loss'] = test_mae_loss
     test_score_df['threshold'] = THRESHOLD
     test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
-    test_score_df['close'] = test[time_steps:].close
+    test_score_df[close_column_name] = test[time_steps:][close_column_name]
 
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=test[time_steps:].date, y=test_score_df.loss,
+    fig.add_trace(go.Scatter(x=test[time_steps:].Date, y=test_score_df.loss,
                         mode='lines',
                         name='Test Loss'))
-    fig.add_trace(go.Scatter(x=test[time_steps:].date, y=test_score_df.threshold,
+    fig.add_trace(go.Scatter(x=test[time_steps:].Date, y=test_score_df.threshold,
                         mode='lines',
                         name='Threshold'))
     fig.update_layout(showlegend=True)
-    fig.show()
+    
+    # Plot mean squared error
+    plot_mae_loss(model, X_train)
+    plot_mae_loss(model, X_test)
 
+    fig.show()
 
     anomalies = test_score_df[test_score_df.anomaly == True]
     anomalies.head()
 
     scaler = StandardScaler()
-    scaler = scaler.fit(train[['close']])
+    scaler = scaler.fit(train[[close_column_name]])
 
-    train['close'] = scaler.transform(train[['close']])
-    test['close'] = scaler.transform(test[['close']])
+    train[close_column_name] = scaler.transform(train[[close_column_name]])
+    test[close_column_name] = scaler.transform(test[[close_column_name]])
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=test[time_steps:].date, y=scaler.inverse_transform(test[time_steps:].close),
+    fig.add_trace(go.Scatter(x=test[time_steps:].Date, y=scaler.inverse_transform(test[time_steps:][close_column_name]),
                         mode='lines',
                         name='Close Price'))
-    fig.add_trace(go.Scatter(x=anomalies.date, y=scaler.inverse_transform(anomalies.close),
+    fig.add_trace(go.Scatter(x=anomalies.Date, y=scaler.inverse_transform(anomalies[close_column_name]),
                         mode='markers',
                         name='Anomaly'))
     fig.update_layout(showlegend=True)
     fig.show()
+    return scaler
 
-def get_and_plot_data(stock="AAPL", start="2010-03-01", end="2021-01-01"):
+def fetch_data(ticker, start_date, end_date):
     """
-    Get and plot data using yfinance library
+    Downloads and writes the stock price data to csv.
+    If the csv data already exists, read from it.
     """
-    stock_obj = yf.Ticker(stock)
-    stock_historical = stock_obj.history(start=start, end=end, interval="1d")
+    file_path = os.path.join("data", ticker+".csv")
+    if os.path.exists(file_path):
+        return pd.read_csv(file_path, index_col=0)
+    start_date_object = to_datetime(start_date)
+    end_date_object = to_datetime(end_date)
+    stock_price = yf.download(ticker, start_date=start_date_object, end_date=end_date_object)
+    stock_price.to_csv(file_path)
+    return pd.read_csv(file_path, index_col=0)
 
-    plt.plot(stock_historical)
-    plt.show()
+def buy_and_hold(stock_price, start_date, end_date):
+    """Calculate profit from buy and hold"""
+    return stock_price.loc[end_date, 'Adj Close'] - stock_price.loc[start_date, 'Open']
 
-    # start_date = st.slider('Enter start date:', value = datetime.datetime(2020,1,1,9,30))
-    # end_date = st.slider('Enter end date:', value = datetime.datetime(2020,1,1,9,30))
-    return stock_historical
 
-def buy_and_hold(stock="AAPL", amt=1, start=datetime.date(2010, 2, 7), end=datetime.date(2021, 2, 7), rounding=False, interval="1d"):
-	'''
-	
-	'''
-	stock_obj = yf.Ticker(stock)
-	start_end = datetime.date(start.year, start.month, start.day + 10)
-	end_end = datetime.date(end.year, end.month, end.day + 10)
-	stock_history_start = stock_obj.history(start=start, end=start_end, interval=interval, rounding=rounding)
-	stock_history_end = stock_obj.history(start=end, end=end_end, interval=interval, rounding=rounding)
-	close_delta = stock_history_end.iloc[len(stock_history_end) - 1, 3] - stock_history_start.iloc[0, 3]
-	return amt * close_delta
-
+def to_datetime(date_str):
+    temp = datetime.strptime(date_str, '%Y-%M-%d')
+    return date(temp.year, temp.month, temp.day)
 
 def calc_max_profit(price_list):
 	'''
@@ -150,22 +154,25 @@ def calc_max_profit(price_list):
 			max_profit += price_list[i + 1] - price_list[i]
 	return max_profit
 
-def plot_mae_loss(model, X_train):
-    X_train_pred = model.predict(X_train)
+def calculate_mae_loss(model, X):
+    X_pred = model.predict(X)
+    mae_loss = np.mean(np.abs(X_pred - X), axis=1)
+    print("[INFO] mae loss:", mae_loss)
+    return mae_loss
 
-    train_mae_loss = np.mean(np.abs(X_train_pred - X_train), axis=1)
-    print("[INFO] mae loss:", train_mae_loss)
-    
-    sns.distplot(train_mae_loss, bins=50, kde=True)
+def plot_mae_loss(model, X_train, bins=50, kde=True):
+    X_train_pred = model.predict(X_train)
+    train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
+    sns.distplot(train_mae_loss, bins=bins, kde=kde)
     plt.show()
 
 def evaluate_model(model, X_test, y_test):
     print("[INFO] Model evaluation results:")
     print(model.evaluate(X_test, y_test))
 
-def train_model(X_train, y_train, epochs=1, time_steps=1):
+def train_model(X_train, y_train, epochs=1):
     """Train model and plot loss and accuracy"""
-    timesteps = X_train.shape[1]
+    time_steps = X_train.shape[1]
     num_features = X_train.shape[2]
 
     # Fit model with early stopping
@@ -185,42 +192,88 @@ def train_model(X_train, y_train, epochs=1, time_steps=1):
     plot_loss(history)
     return model
 
-def load_data(filename='S&P_500_Index_Data.csv', time_steps=1):
-    # read data
-    df = pd.read_csv(os.path.join('data', filename), parse_dates=['date'])
+def plot_and_prepare_data(df, close_column_name="Adj Close"):
 
     # adapted from https://github.com/Tekraj15/AnomalyDetectionTimeSeriesData/blob/master/Anomaly_Detection_Time_Series_Keras.ipynb
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.date, y=df.close,
+    print(df.head())
+    fig.add_trace(go.Scatter(x=df.index, y=df[close_column_name],
                         mode='lines',
-                        name='close'))
+                        name=close_column_name))
     fig.update_layout(showlegend=True)
     fig.show()
 
-    # Preprocess
+    # creating a DataFrame 
+    my_df = {'Date': df.index,  
+            'Adj Close': df[close_column_name]} 
+    df = pd.DataFrame(my_df) 
+    df.reset_index(drop=True, inplace=True)
+    print(df.head())
+
     train_size = int(len(df) * 0.8)
     test_size = len(df) - train_size
     train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
     print(train.shape, test.shape)
 
-    # Go back days for now
-    time_steps = 1
+    scaler = StandardScaler()
+    scaler = scaler.fit(train[[close_column_name]])
 
-    X_train, y_train = create_dataset(train[['close']], train.close, time_steps)
-    X_test, y_test = create_dataset(test[['close']], test.close, time_steps)
+    train[close_column_name] = scaler.transform(train[[close_column_name]])
+    test[close_column_name] = scaler.transform(test[[close_column_name]])
+
+    time_steps = 30
+
+    X_train, y_train = create_dataset(train[[close_column_name]], train[close_column_name], time_steps)
+    X_test, y_test = create_dataset(test[[close_column_name]], test[close_column_name], time_steps)
+
+    print(X_train.shape)
     return X_train, y_train, X_test, y_test, train, test
 
-if __name__ == "__main__":
+def trade_pure_lstm_predictions(model, train, X_test, time_steps=30, close_column_name="Adj Close"):
+    # TODO: Fix IndexError: only integers, slices (`:`), ellipsis (`...`), numpy.newaxis (`None`) and integer or boolean arrays are valid indices
+    scaler = StandardScaler()
+    scaler = scaler.fit(test[[close_column_name]])
+    X_test_pred = model.predict(X_test)
+    inversed_test = scaler.inverse_transform(test[time_steps:][close_column_name])
+    print(inversed_test)
+    
+def run_pipeline(ticker="GME", start_date="2002-02-13", end_date = "2021-02-12"):
+    """For a given stock ticker, 
+    i)  calculate and report profit from buy and hold (see buy_and_hold), 
+    ii) calculate and report from LSTM predictions-based trading (see trade_pure_lstm_predictions)
+    iii) calculate and report profit from anomaly detection-based trading (see detect_anomaly)
+    No more than 1 share is bought/sold at a time.
+    No shorting allowed. i.e we can only sell if we have a share.
+    """
+    print("[INFO] Pipeline started for Ticker:{} Start date:{} End date:{}".format(ticker, start_date, end_date))
 
     # Load data
-    X_train, y_train, X_test, y_test, train, test = load_data(filename = 'S&P_500_Index_Data.csv')
+    # stock_df = get_and_plot_data(ticker, start_date=start_date, end_date=end_date)
+    # print(stock_df)
+    
+    stock_price = fetch_data(ticker, start_date, end_date)
+    buy_and_hold_profit = buy_and_hold(stock_price, start_date, end_date)
+    print("[INFO] Buy and Hold profit is ${}".format(buy_and_hold_profit))
+    X_train, y_train, X_test, y_test, train, test = plot_and_prepare_data(stock_price)
 
     # Train model
     model = train_model(X_train, y_train)
 
-    # Plot mean squared error
-    plot_mae_loss(model, X_train)
-    plot_mae_loss(model, X_test)
+    trade_pure_lstm_predictions(model, train, X_test, time_steps=30)
+
+    # Evaluate model
+    # evaluate_model(model, X_test, y_test)
 
     # Detect anomaly
-    detect_anomaly(test)
+    # detect_anomaly(model, train, test, X_train, X_test, time_steps = 30)
+
+if __name__ == "__main__":
+    # GME start_date=2002-02-13, end_date=2021-02-12
+    run_pipeline(ticker="SPY", start_date="2002-02-13", end_date = "2021-02-12")
+    
+    # S&P start_date=2002-02-13, end_date=2021-02-12
+    # run_pipeline(ticker="SPY", start_date="2010-01-01", end_date = "2020-01-01")
+    
+
+
+   
