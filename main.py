29a8fe6a583b26d915e2dbdb33c1985f0b4e7dbf
ya332 <alparslanyigitcan@gmail.com>
2021-02-15 01:42:35 -0500

    Add figures

--- main.py
+++ main.py
@@ -340,7 +340,7 @@ def trade_pure_lstm_predictions(df, close_column_name="Adj Close"):
     try:
         load_model('lstm-price-predictor')
     except Exception as e:
-        history = model.fit(x_train, y_train, batch_size=1, epochs=1)
+        history = model.fit(x_train, y_train, batch_size=1, epochs=20)
     plt.plot(history.history['loss'])
     plt.title('Model loss')
     plt.ylabel('Mean Squared Error')
@@ -445,7 +445,7 @@ def run_pipeline(ticker="GME", start_date="2002-02-13", end_date = "2021-02-12")
     # buy_and_hold_profit = buy_and_hold(stock_price, start_date, end_date)
     # print("[INFO] Buy and Hold profit is ${}".format(buy_and_hold_profit))
 
-    # lstm_based_strategy_profit, _, _, _ = trade_pure_lstm_predictions(stock_price)
+    lstm_based_strategy_profit, _, _, _ = trade_pure_lstm_predictions(stock_price)
     # print("[INFO] Pure LSTM-based trading profit is ${}".format(lstm_based_strategy_profit))
 
     consolidation_breakout_profit, _, _, _= detect_anomaly(stock_price, time_steps = 30)
@@ -456,15 +456,15 @@ if __name__ == "__main__":
     # S&P start_date=2002-02-13, end_date=2021-02-12
     # [INFO] Pipeline started for Ticker:SPY Start date:2002-02-13 End date:2021-02-12
     # [INFO] Buy and Hold profit is $281.1600112915039
-    # [INFO] Pure LSTM-based trading profit is $110.29022216796918 Profitable trade count: 462 Unprofitable trade count: 401 Success Rate: 53.53418308227115
+    # [INFO] Pure LSTM-based trading profit is $110.29022216796918 Profitable trade count: 462 Unprofitable trade count: 401 Success Rate: 53.53418308227115 Test dataset final RMSE value:8.958006168683683
     # [INFO] Consolidation Breakout trading profit is $4.347592096226596 Profitable trade count: 536 Unprofitable trade count: 395 Success Rate: 57.57250268528465%
     run_pipeline(ticker="SPY", start_date="2002-02-13", end_date = "2021-02-12")
     
     
     # GME start_date=2002-02-13, end_date=2021-02-12
-    # [INFO] Pipeline started for Ticker:GME Start date:2002-02-13 End date:2021-02-12
-    # [INFO] Buy and Hold profit is $45.63333559036254
-    # [INFO] Pure LSTM-based trading profit is $69.04612421989441 Profitable trade count: 213 Unprofitable trade count: 239 Success Rate: 47.123893805309734% Test dataset final RMSE value:13.458590932218822
-    # [INFO] Consolidation Breakout trading profit is $2.1027738249293195 Profitable trade count: 156 Unprofitable trade count: 169 Success Rate: 48.0%
+    # [INFO] Pipeline started for Ticker:GME Start date:2002-02-13 End date:2021-02-12
+    # [INFO] Buy and Hold profit is $45.63333559036254
+    # [INFO] Pure LSTM-based trading profit is $69.04612421989441 Profitable trade count: 213 Unprofitable trade count: 239 Success Rate: 47.123893805309734% Test dataset final RMSE value:13.458590932218822
+    # [INFO] Consolidation Breakout trading profit is $2.1027738249293195 Profitable trade count: 156 Unprofitable trade count: 169 Success Rate: 48.0%
     # run_pipeline(ticker="GME", start_date="2002-02-13", end_date = "2021-02-12")
     
